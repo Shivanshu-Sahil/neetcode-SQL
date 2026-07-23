ebc572b2df83062303c237a72f83a2d0f2ea1290
Shivanshu Sahil <sahil060659@gmail.com>
2026-07-23 18:21:33 +0530

    Add solution for Find Customer Referee

--- LeetCode/Find-Customer-Referee/Solution.sql
+++ LeetCode/Find-Customer-Referee/Solution.sql
@@ -1,31 +1,4 @@
-SELECT
-    name
-FROM
-    Customer
-	
-# SOLUTION 1  - MOST EFFICIENT 
-WHERE 
-    IFNULL(referee_id,0) <> 2;
-    
-#SOLUTOIN 2    
-WHERE
-    referee_id != 2 OR referee_id is NULL
-
-#SOLUTION 3   
-WHERE
-    COALESCE(referee_id, 0) <> 2
-
-#===========================================
-    
-#SOLUTION 4  nested query 
-SELECT  
-    name
-FROM 
-    Customer
-WHERE 
-    id NOT IN
-        (
-        SELECT id
-        FROM Customer
-        WHERE referee_id = 2
-        )
+SELECT name
+FROM Customer
+WHERE COALESCE(referee_id,0) != 2
+;
